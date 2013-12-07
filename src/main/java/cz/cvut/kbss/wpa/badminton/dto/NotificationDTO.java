fe8d8c309684a6e7881d0429bed82fd6924d6cd5
Jan Stepanek <stepaj29@fel.cvut.cz>
2013-12-07 21:59:43 +0100

    DTOs added to reflect model\Entities

---
+++ src/main/java/cz/cvut/kbss/wpa/badminton/dto/NotificationDTO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.cvut.kbss.wpa.badminton.dto;
+
+/**
+ *
+ * @author jan
+ */
+public class NotificationDTO extends AbstractDTO{
+    
+}
