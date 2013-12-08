bbd1979a6dbebef9161d2be0c4947a612f1cbc04
Jan Stepanek <stepaj29@fel.cvut.cz>
2013-12-08 11:20:03 +0100

    PlayerService extends UserService

--- src/main/java/cz/cvut/kbss/wpa/badminton/service/PlayerService.java
+++ src/main/java/cz/cvut/kbss/wpa/badminton/service/PlayerService.java
@@ -8,23 +8,35 @@ package cz.cvut.kbss.wpa.badminton.service;
 
 import cz.cvut.kbss.wpa.badminton.dto.MatchDTO;
 import cz.cvut.kbss.wpa.badminton.dto.PlayerDTO;
+import cz.cvut.kbss.wpa.badminton.model.Player;
 import java.util.List;
 
 /**
  *
  * @author zdeněk
  */
-public interface PlayerService extends UserService{
+public class PlayerService extends UserService implements IPlayerService {    
     
-    public void createPlayerDTO(PlayerDTO dto);
-    
-    public PlayerDTO getPlayer(Long id);
-    
-    public List<PlayerDTO> getAllPlayers();
-    
-    public void enrollPlayerIntoLeague(Long playerId, Long leagueId);
-    
-    public List<MatchDTO> getPlayersMatches();
-    
+    public void createPlayer(PlayerDTO dto) {
+       Player p = new Player();
+       genericDAOIface.saveOrUpdate(p);
+       
+    }
+
+    public PlayerDTO getPlayer(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<PlayerDTO> getAllPlayers() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void enrollPlayerIntoLeague(Long playerId, Long leagueId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<MatchDTO> getPlayersMatches() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- src/main/java/cz/cvut/kbss/wpa/badminton/service/UserService.java
+++ src/main/java/cz/cvut/kbss/wpa/badminton/service/UserService.java
@@ -6,10 +6,21 @@
 
 package cz.cvut.kbss.wpa.badminton.service;
 
+import cz.cvut.kbss.wpa.badminton.dto.UserDTO;
+import cz.cvut.kbss.wpa.dao.GenericDAOIface;
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  *
  * @author jan
  */
-public interface UserService {
+public class UserService implements IUserService{
+    
+    @Autowired
+    public GenericDAOIface genericDAOIface; 
+
+    public void createUser(UserDTO user) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
